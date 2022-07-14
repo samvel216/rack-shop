1ed7246db6322fa211f93b2540880ab4fd7a0751
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-07-14 19:43:33 +0300

    1 нормальный коммит

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import lalka from './templates/render-card';
-import lalka from './templates/render-img-hero';
+const burgerBtn = document.querySelector(".mobile-header-burger-btn");
+const mobileMenu = document.querySelector(".mobile-header-menu-container");
+const mobileClose = document.querySelector(".mobile-header-close-button");
+const baseHeaderContainer = document.querySelector(".base-header-container");
+console.log(mobileMenu);
+const openBurgerBtn = (event) => {
+    event.preventDefault();
+    mobileMenu.style.display = "block";
+    baseHeaderContainer.style.display = "none"
+}
+const closeMobileMenu = (event) => {
+    event.preventDefault();
+    mobileMenu.style.display = "none"
+    baseHeaderContainer.style.display = "flex";
+}
+burgerBtn.addEventListener('click', openBurgerBtn);
+mobileClose.addEventListener('click', closeMobileMenu);
--- src/templates/render-card.js
+++
@@ -1,7 +0,0 @@
-const lalka = () => {
-    console.log("fjdhjfd");
-    console.log("fjdhjfd");
-}
-lalka();
-
-export default lalka;
