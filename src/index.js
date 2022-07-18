6c1f80c6f479d8948b4f0469947b08a7ab0ece9f
samvel216 <75182258+samvel216@users.noreply.github.com>
2022-07-18 16:53:01 +0300

    base-design-2

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ const burgerBtn = document.querySelector(".mobile-header-burger-btn");
 const mobileMenu = document.querySelector(".mobile-header-menu-container");
 const mobileClose = document.querySelector(".mobile-header-close-button");
 const baseHeaderContainer = document.querySelector(".base-header-container");
-console.log(mobileMenu);
+const heroTitleEl = document.querySelector(".hero-title");
+
 const openBurgerBtn = (event) => {
     event.preventDefault();
     mobileMenu.style.display = "block";
-    baseHeaderContainer.style.display = "none"
+    heroTitleEl.style.opacity = "0";
+    mobileMenu.style.transform = "translateY(0%)";
+    console.log(mobileClose);
 }
 const closeMobileMenu = (event) => {
     event.preventDefault();
-    mobileMenu.style.display = "none"
+    mobileMenu.style.transform = "translateY(-150%)"
     baseHeaderContainer.style.display = "flex";
+    heroTitleEl.style.opacity = "1";
+    console.log(baseHeaderContainer);
 }
 burgerBtn.addEventListener('click', openBurgerBtn);
+
+
 mobileClose.addEventListener('click', closeMobileMenu);
